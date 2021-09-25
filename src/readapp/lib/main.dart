09dde7f9bf32dc5feb26f4cb472bd86396422150
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-09-25 18:11:47 +0200

    Add primaryColor to app settings

--- src/readapp/lib/main.dart
+++ src/readapp/lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:readapp/config/typography.dart';
 import 'package:readapp/screens/homepage.dart';
 import 'package:readapp/widgets/main_frame.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: MainFrame(child: HomepageScreen()),
+  runApp(MaterialApp(
+    theme: ThemeData(
+      primaryColor: primaryColor,
+    ),
+    home: const MainFrame(
+      child: HomepageScreen()
+    ),
   ));
 }
 
