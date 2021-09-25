f572a4e641205e8f0c7450996863477781dbde00
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-09-25 18:13:29 +0200

    Add title param to InternalPageFrame class

--- src/readapp/lib/widgets/internal_page_frame.dart
+++ src/readapp/lib/widgets/internal_page_frame.dart
@@ -4,16 +4,20 @@ import 'package:flutter/material.dart';
 class InternalPageFrame extends StatelessWidget{
 
   final Widget body;
+  final String? title;
 
-  const InternalPageFrame({Key? key, required this.body}) : super(key: key);
+  const InternalPageFrame({Key? key, required this.body, this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
+        title: Text(title ?? ''),
+        backgroundColor: Theme.of(context).primaryColor,
       ),
-      body: body,
+      body: SingleChildScrollView(
+        child: body,
+      ),
     );
   }
 
